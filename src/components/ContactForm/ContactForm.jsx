822a4d1900c6f4b908293595eee232cbc0ce8457
maria <fesychmarimia@gmail.com>
2025-04-07 12:29:05 +0200

    try 4th step

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,33 @@
+import { Formik, Form, Field } from "formik";
+import { nanoid } from "nanoid/non-secure";
+import { useId } from "react";
+
 const ContactForm = () => {
-  return <div>ContactForm</div>;
+  const nameFieldId = useId();
+  const phoneFieldId = useId();
+  const contactId = nanoid();
+
+  const initialValues = {
+    username: "",
+    phone: "",
+  };
+
+  const handleSubmit = (values, actions) => {
+    console.log(values);
+    actions.resetForm();
+  };
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      <Form>
+        <label htmlFor={nameFieldId}>Name</label>
+        <Field type="text" name="username" id={nameFieldId}></Field>
+        <label htmlFor={phoneFieldId}>Number</label>
+        <Field type="number" name="phone" id={phoneFieldId}></Field>
+        <button type="submit">Add contact</button>
+      </Form>
+    </Formik>
+  );
 };
 
 export default ContactForm;
